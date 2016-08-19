83644671a5f03556e5ebb61b1b4ac1b678148a61
Alex Ware <alex.ware@gmail.com>
2016-08-19 18:09:23 +0100

    send correct args

--- proxy/main.go
+++ proxy/main.go
@@ -9,14 +9,14 @@ import (
 
 func main() {
 	path := "/home/pi/433Utils/RPi_utils/codesend"
-	args := []string{"7289615"}
+	args := []string{"codesend", "7289615"}
 	env := os.Environ()
 	err := syscall.Exec(path, args, env)
 	if err != nil {
 		fmt.Println(err)
 	}
 	time.Sleep(1e10)
-	argsOff := []string{"7289607"}
+	argsOff := []string{"codesend", "7289607"}
 	err = syscall.Exec(path, argsOff, env)
 	if err != nil {
 		fmt.Println(err)
