13a95295ef105f3d55058a0ca7f3c0b39a0831fc
Alex Ware <alex.ware@gmail.com>
2016-08-19 21:23:57 +0100

    basic web stuff

--- main.go
+++ main.go
@@ -2,18 +2,42 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
 )
 
 func main() {
-	switch1 := statusSwitch{7289615, 7289607}
-	err := switch1.On()
-	if err != nil {
-		fmt.Println(err)
+	http.HandleFunc("/switch/", switchHandler())
+	http.Handle("/", http.FileServer(http.Dir("./public/")))
+	fmt.Println("HANDLING HTTP")
+	http.ListenAndServe(":8080", nil)
+}
+
+func switchHandler() func(w http.ResponseWriter, r *http.Request) {
+	switches := map[string]statusSwitch{"one": statusSwitch{7289615, 7289607}}
+	fmt.Println("Making handler.")
+	return func(w http.ResponseWriter, r *http.Request) {
+		k := r.PostFormValue("switch")
+		action := r.PostFormValue("action")
+		fmt.Println(k, action)
+		s, exists := switches[k]
+		if !exists {
+			fmt.Fprintln(w, "¯\\_(ツ)_/¯")
+			return
+		}
+		if action == "on" {
+			s.On()
+			fmt.Fprintln(w, "👍💡")
+			return
+		}
+		if action == "off" {
+			s.Off()
+			fmt.Fprintln(w, "👍𝍈")
+			return
+		}
+		fmt.Fprintln(w, "¯\\_(ツ)_/¯")
+		return
 	}
-	time.Sleep(1e10)
-	err = switch1.Off()
-	if err != nil {
-		fmt.Println(err)
-	}
+}
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "HELLO")
 }
