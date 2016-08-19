de28872955c4dad72602b6fc1a7be3f6a4800fe7
Alex Ware <alex.ware@gmail.com>
2016-08-19 17:55:11 +0100

    Call rpiutils codesend from go

---
+++ proxy/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+	"time"
+)
+
+func main() {
+	path := "/home/pi/433Utils/RPi_utils/codesend"
+	args := []string{"7289615"}
+	env := os.Environ()
+	err := syscall.Exec(path, args, env)
+	if err != nil {
+		fmt.Println(err)
+	}
+	time.Sleep(1e10)
+	argsOff := []string{"7289607"}
+	err = syscall.Exec(path, argsOff, env)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
