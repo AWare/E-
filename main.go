df823395a4d9753b3482008e8f0e9051ba59e16c
Alex Ware <alex.ware@gmail.com>
2016-12-14 23:15:14 +0000

    disable some switches for a bit

--- main.go
+++ main.go
@@ -13,12 +13,12 @@ func main() {
 	switches := [...]statusSwitch{
 		statusSwitch{os.Getenv("ONEON"), os.Getenv("ONEOFF"), "🌇"},
 		statusSwitch{os.Getenv("TWOON"), os.Getenv("TWOOFF"), "💡"},
-		statusSwitch{os.Getenv("THREEON"), os.Getenv("THREEOFF"), "🔦"},
-		statusSwitch{os.Getenv("FOURON"), os.Getenv("FOUROFF"), "🛰 "},
+		//statusSwitch{os.Getenv("THREEON"), os.Getenv("THREEOFF"), "🔦"},
+		//statusSwitch{os.Getenv("FOURON"), os.Getenv("FOUROFF"), "🛰 "},
 		statusSwitch{os.Getenv("ONEAON"), os.Getenv("ONEAOFF"), "👍🏻"},
 		statusSwitch{os.Getenv("TWOAON"), os.Getenv("TWOAOFF"), "🐕"},
-		statusSwitch{os.Getenv("THREEAON"), os.Getenv("THREEAOFF"), "🌎"},
-		statusSwitch{os.Getenv("FOURAON"), os.Getenv("FOURAOFF"), "📬"},
+		//statusSwitch{os.Getenv("THREEAON"), os.Getenv("THREEAOFF"), "🌎"},
+		//statusSwitch{os.Getenv("FOURAON"), os.Getenv("FOURAOFF"), "📬"},
 		//	statusSwitch{os.Getenv("AON"), os.Getenv("AOFF"), "🎤"},
 	}
 	switchMap := make(map[string]switcher)
